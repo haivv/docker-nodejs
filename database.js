95144070d15496c1ab66f526e20775062b6544c5
Van-Hai Vu <50228260+haivv@users.noreply.github.com>
2024-04-27 00:42:10 +0900

    Update database.js

--- database.js
+++ database.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql');
 
 // Thông tin kết nối từ biến môi trường hoặc docker-compose.yml
-const host = process.env.MYSQL_HOST || '127.0.0.1'; //
-const port = process.env.MYSQL_PORT || '3307';
+const host = process.env.MYSQL_HOST || 'mdb';
+const port = process.env.MYSQL_PORT || '3306';
 const user = process.env.MYSQL_USER || 'root';
-const password = process.env.MYSQL_PASSWORD || 'Password123!'; // Thay đổi thành mật khẩu root của MariaDB
-const database = process.env.MYSQL_DB || 'mariadb_is_awesome'; // Thay đổi thành tên cơ sở dữ liệu bạn đã tạo trong docker-compose.yml
-
+const password = process.env.MYSQL_PASSWORD || 'Password123!';
+const database = process.env.MYSQL_DB || 'mariadb_is_awesome';
 
 const connection = mysql.createConnection({
   host: host,
@@ -16,13 +15,11 @@ const connection = mysql.createConnection({
   database: database
 });
 
-
 connection.connect(function (error) {
     if (error) {
-        console.error('Error connecting to MySQL:', error.stack);
+        console.error('Error connecting to MySQL:', error.message);
         dbConnectionStatus = 'Disconnected';
-    }
-    else {
+    } else {
         console.log('Connected to MySQL as id', connection.threadId);
         dbConnectionStatus = 'Connected';
     }
